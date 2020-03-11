54245647c1edf416bcc0e6371bafd3831a3b0274
Nahid fatima <fatimanahid04@gmail.com>
2020-03-11 14:38:09 +0530

    songs Api

--- index.js
+++ index.js
@@ -7,104 +7,119 @@ let port = process.env.PORT || 4500;
 //console.log(express);
 
 
-let users =[
+
+
+let songs = [
     {
-    id:1,
-    name:"John Doe"
+      id:1,    
+      song_name:"shape of you",
+      singre_name:"Ed Sheeran",
+      album_name:"shape of you"
     },
 
     {
+        id:2,   
+        Song_name:"Despacito",
+        singre_name:"luis fonsi/justin bieber",
+        album_name:"despacito"
 
-    id:2,
-    name:"Emma Doe"
     },
 
     {
-    id:3,
-    name:"Ajay Doe"
+        id:3,   
+        song_name:"love yourself",
+        singre_name:"justin bieber",
+        album_name:"purpose"
     },
      
     {
-    id:4,
-    name:"MANISH CHANDWANI"
-
+        id:4,  
+        song_name:"on my way",
+        singre_name:"Alan Walker",
+      album_name:"on my way"
+    },
+    {
+        id:5,  
+        song_name:"let me love you",
+        singre_name:"DJ Snake",
+        album_name:"Encore"
     }
 
 ];
 
-
-
-
-//get
-
-app.get("/api/user", (req,res) => {
-    res.send("hello user")
-});
-
-app.get("/api/courses",(req,res) => {
-    res.send(["Angular","reactjs","nodejs"])
+app.get("/api/songs/all_list", (req, res) =>{
+       res.send(songs)
 });
 
 
-//app.get("/api/:year/:month",(req,res) => {
-//  res.send(req.params);
-//});
+app.get("/api/songs/song_by_id/:id", (req, res) =>{
+    let song = songs.find(item => item.id === parseInt( req.params.id));
+    if(!song) { return res.status(404).send({message:"invalid song"})};
+    res.send(song);
+})
 
-
-app.get("/api/users", (req, res) => {
-    res.send(users);
-});
-
-app.get("/api/users/:id", (req, res) =>{
-    let user = users.find(item =>item.id === parseInt( req.params.id));
-    if(!user) { return res.status(404).send({message:"invalid user"})};
-    res.send(user);
-});
-
-
-//create a new user
-app.post("/api/users/newuser",(req, res) =>  {
+//create a new song
+app.post("/api/songs/create_song/", ( req, res) => {
     let schema = joi.object({
-        name: joi.string().min(3).max(100).required().regex(),
-  });
-    let result = validationError(req.body);
-    let {error} = result;
-   //console.log(result); 
-   if(error) {return res.send(error.details[0].message)}
-    let user = {
-        id:users.length + 1,
-        name:req.body.name
+        song_name: joi.string().min(3).max(100).required(),
+        singre_name: joi.string().min(3).max(100).required(),
+        album_name: joi.string().min(3).max(100).required(),
+
+      });
+      let result = validationError(req.body);
+      let {error} = result;
+      console.log(result); 
+      if(error) {return res.send(error.details[0].message)}
+    let song = {
+        id:songs.length + 1,
+        song_name:req.body.song_name,
+        singre_name:req.body.singre_name,
+        album_name:req.body.album_name
     
     };
-    users.push(user);
-    res.send(users);
-}); 
+    songs.push(song);
+    res.send(songs);
+});
 
-
-//update the user
-app.put("api/user/updateuser/id", (req, res) => {
-    let user = users.find(data => data.id === parseInt (req.params.id));
-    if(!user){return res.status(404).send(
-        { message:"Invalid user id"}) };
+//update a song
+app.put("/api/songs/update_song/:id", ( req, res) => {
+    let song = songs.find(item => item.id === parseInt( req.params.id));
+    if(!song) { return res.status(404).send({message:"invalid song"})};
     let schema = joi.object({
-        name: joi.string().min(3).max(100).required().regex(),
-  });
+        song_name: joi.string().min(3).max(100).required(),
+        singre_name: joi.string().min(3).max(100).required(),
+        album_name: joi.string().min(3).max(100).required()
 
+      });
+      
     let result = validationError(req.body);
     let {error} = result;
-    //console.log(result); 
-     if(error) {return res.send(error.details[0].message)}
-    user.name = req.body.name;
-    res.send(users);
+    if(error) {return res.send(error.details[0].message)};
+    song.song_name = req.body.song_name;
+    res.send(songs);
 
+
+})
+
+//Delete the song
+app.delete("/api/songs/remove_song/:id", ( req, res ) => {
+    let song = songs.find(data => data.id === parseInt(req.params.id));
+    if (!song) { return res.status(404).send({ message:"Invalid id"})};
+    let index = songs.indexOf(song);
+    console.log(index); 
+    songs.splice( index, 1); 
+    res.send(songs); 
 });
 
 function  validationError(error){
     let schema = joi.object({
-        name: joi.string().min(3).max(100).required().regex(),
+        song_name: joi.string().min(3).max(100).required(),
+        singre_name: joi.string().min(3).max(100).required(),
+        album_name: joi.string().min(3).max(100).required()
   });
 
   return schema.validate(error);
 }
 
+//
 app.listen(port, () => console.log(`port is working on ${port}`)); 
---
+++ main.js
@@ -0,0 +1,120 @@
+ let express = require("express");
+let app = express();
+let joi = require("@hapi/joi");
+app.use(express.json()); 
+//console.log(process);
+let port = process.env.PORT || 4500;
+//console.log(express);
+
+
+let users =[
+    {
+    id:1,
+    name:"John Doe"
+    },
+
+    {
+
+    id:2,
+    name:"Emma Doe"
+    },
+
+    {
+    id:3,
+    name:"Ajay Doe"
+    },
+     
+    {
+    id:4,
+    name:"MANISH CHANDWANI"
+
+    }
+
+];
+
+
+
+
+//get
+
+app.get("/api/user", (req,res) => {
+    res.send("hello user")
+});
+
+app.get("/api/courses",(req,res) => {
+    res.send(["Angular","reactjs","nodejs"])
+});
+
+
+//app.get("/api/:year/:month",(req,res) => {
+//  res.send(req.params);
+//});
+
+
+app.get("/api/users", (req, res) => {
+    res.send(users);
+});
+
+app.get("/api/users/:id", (req, res) =>{
+    let user = users.find(item =>item.id === parseInt( req.params.id));
+    if(!user) { return res.status(404).send({message:"invalid user"})};
+    res.send(user);
+});
+
+
+//create a new user
+app.post("/api/users/newuser",(req, res) =>  {
+    let schema = joi.object({
+      name: joi.string().min(3).max(100).required(),
+    });
+    let result = validationError(req.body);
+    let {error} = result;
+    console.log(result); 
+    if(error) {return res.send(error.details[0].message)}
+    let user = {
+        id:users.length + 1,
+        name:req.body.name
+    
+    };
+    users.push(user);
+    res.send(users);
+}); 
+
+
+//update the user
+app.put("/api/user/updateuser/:id", (req, res) => {
+    let user = users.find(data => data.id === parseInt(req.params.id));
+    if(!user){return res.status(404).send(
+        { message:"Invalid user id"}) };
+    let schema = joi.object({
+        name: joi.string().min(3).max(100).required()
+     });
+
+    let result = validationError(req.body);
+    let {error} = result;
+    //console.log(result); 
+     if(error) {return res.send(error.details[0].message)};
+    user.name = req.body.name;
+    res.send(users);
+
+});
+
+//Delete the user
+app.delete("/api/user/removeuser/:id", (req, res) =>{
+    let user = users.find(data => data.id === parseInt(req.params.id));
+    if (!user) { return res.status(404).send({ message:"Invalid id"})};
+    let index = users.indexOf(user);
+    console.log(index); 
+    users.splice( index, 1); 
+    res.send(users); 
+});
+
+function  validationError(error){
+    let schema = joi.object({
+        name: joi.string().min(3).max(100).required(),
+  });
+
+  return schema.validate(error);
+}
+
+app.listen(port, () => console.log(`port is working on ${port}`)); 
